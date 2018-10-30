88c1d2702d49ff7bfe11459ba86832c4a3c3d9fd
David Garcia <david_r_garcia@homedepot.com>
2018-10-30 15:53:05 -0400

    Updated schemas

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 const { ApolloServer, gql } = require('apollo-server');
-const importSchema = require('graphql-import').importSchema()
-const makeExecutableSchema = require('graphql-tools').makeExecutableSchema()
+const importSchema = require('graphql-import').importSchema
+const makeExecutableSchema = require('graphql-tools').makeExecutableSchema
 
 const typeDefs = importSchema('Schemas/schema.graphql')
 const resolvers = {}
 
-
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers , mocks: true});
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
