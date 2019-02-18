4e179457a34464edc2fac2b7cc50f0de99832038
David Garcia <david_r_garcia@homedepot.com>
2019-02-18 14:05:33 -0500

    Project_membership schema

---
+++ Schemas/project_membership.js
@@ -0,0 +1,32 @@
+module.exports.typedef = `
+type Project_Membership {
+    id: ID!
+    Person: Person!
+    role: Role!
+    email: String
+    name: String
+    Initials: String
+    project_color: String
+    favorite: Boolean
+}
+`
+
+module.exports.resolvers = {
+    Query: {
+        Project_membership: async (_source, { id }, { dataSources }) => {
+            return dataSources.trackerAPI.getProjects(id);
+        },
+    },
+    Project_membership: {
+        epics: async (_source, args, { dataSources }) => {
+            return dataSources.trackerAPI.getEpics(_source.id);
+        },
+        stories: async (_source, args, { dataSources }) => {
+            return dataSources.trackerAPI.getStories(_source.id);
+        },
+        labels: async (_source, args, { dataSources }) => {
+            return dataSources.trackerAPI.getLabels(_source.id);
+        },
+    }
+
+}
